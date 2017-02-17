ea55e9af93e877ecee787a258a539a2d7f3ddecf
Cassie Musolf <cassiemusolf@gmail.com>
2017-02-17 09:28:14 -0800

    add first test to return one letter to the user, get test to pass and then make it fail to ensure it's working correctly.

---
+++ Objects/WordCounter.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace WordCounterApp.Objects
+
+{
+  public class RepeatCounter
+  {
+    public string _userString;
+
+    //Getter
+    public string GetUserString()
+    {
+      return _userString;
+    }
+
+    //Constructor
+    public RepeatCounter(string newUserString)
+    {
+      _userString = newUserString;
+    }
+
+    //Main Method
+    public string CountRepeats()
+    {
+      return "b";
+    }
+  }
+}
---
+++ Tests/WordCounterTests.cs
@@ -0,0 +1,20 @@
+using Xunit;
+using WordCounterApp.Objects;
+
+namespace WordCounterApp.Objects
+{
+  public class RepeatCounterTest
+  {
+    [Fact]
+    public void CountRepeats_ReturnSingleChar_Char()
+    {
+      //Arrange
+      string oneChar = "a";
+      RepeatCounter testRepeatCounter = new RepeatCounter(oneChar);
+      //Act
+      string result = testRepeatCounter.CountRepeats();
+      //Assert
+      Assert.Equal("a", result);
+    }
+  }
+}
